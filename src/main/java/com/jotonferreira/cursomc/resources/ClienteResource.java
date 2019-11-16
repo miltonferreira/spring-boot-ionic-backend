3446b6c5c8be88409c5421a929fcf0872b2aa76d
miltonferreira <jotonferreira@gmail.com>
2019-11-16 20:50:28 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/jotonferreira/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jotonferreira/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jotonferreira.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jotonferreira.cursomc.domain.Cliente;
-import com.jotonferreira.cursomc.domain.Cliente;
 import com.jotonferreira.cursomc.dto.ClienteDTO;
+import com.jotonferreira.cursomc.dto.ClienteNewDTO;
 import com.jotonferreira.cursomc.services.ClienteService;
 
 /*
@@ -43,54 +45,68 @@ public class ClienteResource {
 		
 	}
 	
+	// HTTP = 201 indica que foi criado nova categoria
+	@RequestMapping(method = RequestMethod.POST) // indica que é uma inserção de nova categoria
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){ // @RequestBody faz obj Json ser convertido para java - @Valid indica que existe requisitos para add nova categoria
+			
+		Cliente obj = service.fromDTO(objDto); // pegas as infos do CategoriaDTO e cria uma Categoria sem lista de produtos associados
+			
+		obj = service.insert(obj);
+			
+		// cria o caminho URL do ID da nova categoria - ex: http://localhost:8080/categorias/3 << 3 é a nova categoria
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+			
+		return ResponseEntity.created(uri).build(); // retorna resposta 202 do HTTP
+	}
+	
 	// HTTP = 204 No Content indica que foi atualizado categoria
-		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-		public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
 			
-			Cliente obj = service.fromDTO(objDto); // pegas as infos do ClienteDTO e cria uma Cliente sem lista de produtos associados
+		Cliente obj = service.fromDTO(objDto); // pegas as infos do ClienteDTO e cria uma Cliente sem lista de produtos associados
 			
-			obj.setId(id); // pega a id da categoria
+		obj.setId(id); // pega a id da categoria
 			
-			obj = service.update(obj);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build(); // retorna um conteudo vazio por ser void
+	}
+		
+	//Controlador REST, encontra uma categoria com id indicado
+		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+		public ResponseEntity<Void> delete(@PathVariable Integer id) {
+				
+			service.delete(id); // escolhe id da categoria que vai deletar
+				
 			return ResponseEntity.noContent().build(); // retorna um conteudo vazio por ser void
+				
 		}
 		
-		//Controlador REST, encontra uma categoria com id indicado
-			@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-			public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		//Controlador REST, encontra uma lista de categorias
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List<ClienteDTO>> findAll() {
 				
-				service.delete(id); // escolhe id da categoria que vai deletar
+			List<Cliente> list = service.findAll();
 				
-				return ResponseEntity.noContent().build(); // retorna um conteudo vazio por ser void
+			List<ClienteDTO> listDtio = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); // transforma categorias em categoriasDto
 				
-			}
+			return ResponseEntity.ok().body(listDtio); // retorna a lista de categoriasDto
+				
+		}
 			
-			//Controlador REST, encontra uma lista de categorias
-			@RequestMapping(method=RequestMethod.GET)
-			public ResponseEntity<List<ClienteDTO>> findAll() {
-				
-				List<Cliente> list = service.findAll();
-				
-				List<ClienteDTO> listDtio = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); // transforma categorias em categoriasDto
-				
-				return ResponseEntity.ok().body(listDtio); // retorna a lista de categoriasDto
-				
-			}
-			
-			//Controlador REST, encontra uma lista de categorias
-			@RequestMapping(value="/page", method=RequestMethod.GET)
-			public ResponseEntity<Page<ClienteDTO>> findPage(
-					@RequestParam(value="page", defaultValue="0") Integer page, // @RequestParam é um parametro opcional
-					@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, //linesPerPage = linhas por pagina
-					@RequestParam(value="orderBy", defaultValue="id") String orderBy, //nome indica qual campo ordena a lista
-					@RequestParam(value="direction", defaultValue="ASC") String direction) { // ASC = ordenação ascedente
+		//Controlador REST, encontra uma lista de categorias
+		@RequestMapping(value="/page", method=RequestMethod.GET)
+		public ResponseEntity<Page<ClienteDTO>> findPage(
+				@RequestParam(value="page", defaultValue="0") Integer page, // @RequestParam é um parametro opcional
+				@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, //linesPerPage = linhas por pagina
+				@RequestParam(value="orderBy", defaultValue="id") String orderBy, //nome indica qual campo ordena a lista
+				@RequestParam(value="direction", defaultValue="ASC") String direction) { // ASC = ordenação ascedente
 						
-				Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction); //
+			Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction); //
 						
-				Page<ClienteDTO> listDtio = list.map(obj -> new ClienteDTO(obj)); // transforma categorias em paginação
+			Page<ClienteDTO> listDtio = list.map(obj -> new ClienteDTO(obj)); // transforma categorias em paginação
 						
-				return ResponseEntity.ok().body(listDtio); // retorna a lista de categoriasDto
+			return ResponseEntity.ok().body(listDtio); // retorna a lista de categoriasDto
 						
-			}
+		}
 	
 }
--- src/main/java/com/jotonferreira/cursomc/services/ClienteService.java
+++ src/main/java/com/jotonferreira/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jotonferreira.cursomc.domain.Cidade;
 import com.jotonferreira.cursomc.domain.Cliente;
+import com.jotonferreira.cursomc.domain.Endereco;
+import com.jotonferreira.cursomc.domain.enums.TipoCliente;
 import com.jotonferreira.cursomc.dto.ClienteDTO;
+import com.jotonferreira.cursomc.dto.ClienteNewDTO;
 import com.jotonferreira.cursomc.repositories.ClienteRepository;
+import com.jotonferreira.cursomc.repositories.EnderecoRepository;
 import com.jotonferreira.cursomc.services.exceptions.DataIntegrityException;
 import com.jotonferreira.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -27,6 +33,9 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;					//Classe é interface
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	//Metodo que procura o obj pelo id indicado
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -35,6 +44,17 @@ public class ClienteService {
 				"Objeto não encontrado! id: " + id + ", Tipo: " + Cliente.class.getName()));	
 	}
 	
+	// salva uma nova cliente no BD
+	@Transactional // 
+	public Cliente insert(Cliente obj) {
+		
+		obj.setId(null);
+		obj = repo.save(obj); // se tiver um id será uma atualização e não um novo cliente
+		
+		enderecoRepository.saveAll(obj.getEnderecos()); // salva os endereços do cliente
+		
+		return obj;
+	}
 	
 	// atualiza a cliente
 	public Cliente update(Cliente obj) {
@@ -85,6 +105,29 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(),objDto.getEmail(), null, null);
 		
+	}
+	
+	// instancia um novo cliente pegando um ClienteDTO com infos pessoais, endereço e telefones
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 }
